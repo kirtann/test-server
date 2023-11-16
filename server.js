993cf28f4f7236bd46db4fed4e75c8b7beb19911
kirtann <94744255+kirtann@users.noreply.github.com>
2023-11-16 19:52:41 +0530

    updated package.json

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
-import express from 'express';
-import { connect, Schema, model } from 'mongoose';
-import { json } from 'body-parser';
-import cors from 'cors';
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
-app.use(json());
+app.use(bodyParser.json());
 
 // MongoDB connection
-connect('mongodb+srv://kirtan_14:Kirtanjain1234.@cluster-kirtan.bw7og2l.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://kirtan_14:Kirtanjain1234.@cluster-kirtan.bw7og2l.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 // Define MongoDB Schema and Model for User
-const userSchema = new Schema({
+const userSchema = new mongoose.Schema({
   username: String,
   password: String,
 });
 
-const User = model('User', userSchema);
+const User = mongoose.model('User', userSchema);
 
 // Routes
 app.post('/api/signup', async (req, res) => {
